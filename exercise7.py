e3b82a8808bfcb2bef766b85b76615c996367ee2
mykangelo <mmykangelo@gmail.com>
2024-01-22 18:00:51 +0800

    Answer for exercise number 7

---
+++ exercise7.py
@@ -0,0 +1,12 @@
+
+
+
+#store the given statement to the string variable
+str_x = "Emma is good developer. Emma is a writer"
+
+#create a count function for the given string to find how many times the substring "Emma" appears
+how_many_emma = str_x.count("Emma")
+print("Emma appeared" , how_many_emma , "times")
+
+
+
